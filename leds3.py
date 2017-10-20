62112fd6d9a81c6e9d9036cbc32c98831096f82b
jacob-jarick <mem.namefix@gmail.com>
2017-10-20 22:02:32 +1000

    fix

--- leds3.py
+++ leds3.py
@@ -4,25 +4,24 @@ GPIO.setmode(GPIO.BOARD)
 
 def fade():
   for number in range(100):
-    led = 1
+    led = 0
+    time.sleep(0.0001)
     for px in pinlist:
+      led += 1
       dc =  ((led * spacer) + i ) % 100
       if number > dc:
         GPIO.output(px,GPIO.HIGH)
       else:
         GPIO.output(px,GPIO.LOW)
 
-      led += 1
-      time.sleep(0.0001)
-
 pinlist = [7, 15, 29, 35, 33]
 led_count = len(pinlist)
 spacer = ( 100 / led_count )
-i = 1
 
 for px in pinlist:
   GPIO.setup(px, GPIO.OUT)
 
+i = 1
 try:
   while True:
     i = i + 1
@@ -30,7 +29,6 @@ try:
 
 except KeyboardInterrupt:
   pass
-
 
 finally:
   GPIO.cleanup() # this ensures a clean exit
